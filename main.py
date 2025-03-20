4118a2433d7de029d1b1498ad6b161c61cd5aeae
Patrick Patten <joeypatten8@gmail.com>
2025-03-20 08:50:36 -0300

    Create main application file

---
+++ main.py
@@ -0,0 +1,14 @@
+# Lambda Studios
+# C: 2025-03-20
+# M: 2025-03-20
+# Backend Main File
+
+def main():
+    '''
+    Main Function. Hello world!
+    :return:
+    '''
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
